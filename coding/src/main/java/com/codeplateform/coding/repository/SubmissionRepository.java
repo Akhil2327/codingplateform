aba06999680b72cc1a7ca5759c278cf52d61e087
Akhil2327 <akhil242rawat@gmail.com>
2025-04-22 00:59:22 +0530

    initial commit

--- coding/src/main/java/com/codeplateform/coding/repository/SubmissionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeplateform.coding.repository;
-
-import com.codeplateform.coding.entity.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
-
